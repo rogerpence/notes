d22e8cf367808f9956fd368f1cbbfafba4521325
Roger Pence <roger.pence@gmail.com>
2020-11-20 10:38:44 -0600

    Initial commit

---
+++ data.js
@@ -0,0 +1,111 @@
+const data = [{
+        title: "Intro",
+        text: [
+            "Thank you",
+            "How to give new life to your business critical IBM i RPG applications on .NET",
+            "Motivations and needs",
+            "Considerations and recommendations"
+        ]
+    },
+    {
+        title: "Don't jump to conclusions",
+        text: [
+            "Migration polarizing topic",
+            "SPACEBAR",
+            "Not about eliminating IBM i",
+            "Rather supplementing IBM i",
+            "SPACEBAR",
+            "Giving your RPG a new home where it can be dramatically and easly extended",
+            "But also let the IBM i continue to do what it does so well no"
+        ]
+    },
+    {
+        title: "IBM i decision makers today face many challenges",
+        text: [
+            "RPG applications need to do more modern tasks",
+            "Apps grew in simpler times",
+            "Integrate with more varied hardware",
+            "Better web services and web sites",
+            "Mobile apps",
+            "Integrate with other DB platforms--mySQL, PostgreSQL, ORACLE, NOSQL...",
+            "PAUSE",
+            "Users, managment and business partners still have greens-screen fatigue",
+            "Many newly-appointed IT directors aren't familiar with IBM i",
+            "PAUSE",
+            "RPG talent is getting hard to find",
+            "Attracting younger programmers to RPG is challenging",
+            "PAUSE",
+            "RPG apps are rarely documented well",
+            "Programmers who understand them best are nearing retirement"
+        ]
+    },
+    {
+        title: "Deeply depend on apps",
+        text: [
+            "2020 IBM i Midrange Markplace Survey says 74% have home-brewed RPG apps",
+            "In nearly every case... bedrock applications created by RPG teams decades ago",
+            "Started simple but quickly grew into pervasive monolith that governs most, if not all, aspects of the business",
+            "Decades later, business deeply depends on these apps",
+            "ensure the delivery of unique business value to customers and business partners",
+        ]
+    },
+    {
+        title: "Most RPG programmers retiring",
+        text: [
+            "Perhaps the largest problem decision-makers face is that RPG programmers are starting to retire",
+            "Most are rapidly approaching retirement age",
+            "very bad news for businesses with RPG applications.",
+            "businesses depend on their RPG programmers to maintain and enhance their RPG applications.",
+            "Their business depends deeply on these RPG programs. With RPG programmers, it will be very challenging to maintain that RPG. Our customers and prospects are very concerned about this."
+        ]
+    },
+    {
+        title: "This is RPG's new reality",
+        text: [
+            "pundits - RPG demise for years",
+            "Like COBOL, RPG will be with us for years",
+            "Pundits were wrong, RPG isn't going away",
+            "But RPG programmers are",
+            "Consider the plight of New Jersey in the US in April 2020.",
+            "Couldn't print unemployment benefit checks because its COBOL application broke.",
+            "The state didn't have any COBOL programmers available to fix the application. A similar issue also happened in Oklahoma.",
+            "RPG and COBOL were both introduced in 1959.",
+            "RPG shops will soon have simliar problems."
+        ]
+    },
+    {
+        title: "It's a well-founded concern. ",
+        text: [
+            "",
+            "",
+        ]
+    },
+    {
+        title: "",
+        text: [
+            "",
+            "",
+        ]
+    },
+    {
+        title: "",
+        text: [
+            "",
+            "",
+        ]
+    },
+    {
+        title: "",
+        text: [
+            "",
+            "",
+        ]
+    },
+    {
+        title: "",
+        text: [
+            "",
+            "",
+        ]
+    },
+]
---
+++ notes.js
@@ -0,0 +1,14 @@
+const mainEntry = () => {
+    const main = {
+        data() {
+            return {
+                data: data
+            }
+        }
+    }
+    Vue.createApp(main).mount('#app');
+}
+
+mainEntry();
+
+export { mainEntry };
